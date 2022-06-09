c76a043324b77d4eea34d8dd63424d06ea06b759
Migz <juanity.andoy@gmail.com>
2022-06-09 22:08:10 +0800

    change firebase json structure add borrower

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -20,6 +20,7 @@ export class LoginPage implements OnInit {
     private router: Router,
     private loadingController: LoadingController,
     private dataService: DataService,
+    private alertController: AlertController,
   ) { }
 
   ngOnInit() {
@@ -36,13 +37,25 @@ export class LoginPage implements OnInit {
 
     const formData = this.loginForm.value;
 
-    this.dataService.getUsers().subscribe(res => {
+    this.dataService.getUsers().subscribe(async res => {
       const result = res.filter((user) => user.user === formData.user && user.password === formData.password);
       if (result.length > 0) {
         this.router.navigate(['home']);
-      } else {
-        console.log('wrong');
+        return;
       }
+
+      let alert = await this.alertController.create({
+        subHeader: "Authetication Failed",
+        message: "Wrong credentials. Please try again.",
+        backdropDismiss: false,
+        buttons: [{
+          text: "OK",
+          handler: () => {
+            alert.dismiss();
+          }
+        }],
+      });
+      alert.present();
     });
 
     loading.dismiss();
